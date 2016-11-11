a78428865815e08bdd71df85312ff1706916610e
Kyle Hall <hallkd47@gmail.com>
2016-11-10 21:20:48 -0600

    fixes bug where running total was reset to zero on each coin inserted

--- src/main/scala/com/kylehall/vendingmachine/VendingMachine.scala
+++ src/main/scala/com/kylehall/vendingmachine/VendingMachine.scala
@@ -2,13 +2,13 @@ package com.kylehall.vendingmachine
 
 class VendingMachine {
 
-  def insertCoin(coin: String): Float = {
-    var total = 0.0f
-    if (coin == "dime") total += 0.10f
-    else if (coin == "nickel") total += 0.05f
-    else if (coin == "quarter") total += 0.25f
-    else if (coin == "penny") total += 0.0f
-    total
+  def insertCoin(coin: String, currentTotal: Float): Float = {
+    var newTotal = currentTotal
+    if (coin == "dime") newTotal += 0.10f
+    else if (coin == "nickel") newTotal += 0.05f
+    else if (coin == "quarter") newTotal += 0.25f
+    else if (coin == "penny") newTotal += 0.0f
+    newTotal
   }
 
 }
--- src/test/scala/com/kylehall/vendingmachinetest/VendingMachineTest.scala
+++ src/test/scala/com/kylehall/vendingmachinetest/VendingMachineTest.scala
@@ -7,30 +7,30 @@ class VendingMachineSpec extends UnitSpec {
   def vendingMachine = new VendingMachine()
 
   "A VendingMachine" should "accept dimes and value them at 10 cents" in {
-    val total = vendingMachine.insertCoin("dime")
+    val total = vendingMachine.insertCoin("dime", 0.0f)
     assert(total == 0.10f)
   }
 
   it should "accept nickels and value them at 5 cents" in {
-    val total = vendingMachine.insertCoin("nickel")
+    val total = vendingMachine.insertCoin("nickel", 0.0f)
     assert(total == 0.05f)
   }
 
   it should "accept quarters and value them at 25 cents" in {
-    val total = vendingMachine.insertCoin("quarter")
+    val total = vendingMachine.insertCoin("quarter", 0.0f)
     assert(total == 0.25f)
   }
 
   it should "reject pennies and value them at 0 cents" in {
-    val total = vendingMachine.insertCoin("penny")
+    val total = vendingMachine.insertCoin("penny", 0.0f)
     assert(total == 0.0f)
   }
 
   it should "accept multiple coins and keep running total" in {
     var total = 0.0f
-    total += vendingMachine.insertCoin("dime")
+    total = vendingMachine.insertCoin("dime", total)
     assert(total == 0.10f)
-    total += vendingMachine.insertCoin("quarter")
+    total = vendingMachine.insertCoin("quarter", total)
     assert(total == 0.35f)
   }
 

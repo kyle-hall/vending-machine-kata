158b6f4df2625a30e51e1482592aabebbdc23169
Kyle Hall <hallkd47@gmail.com>
2016-11-10 20:22:32 -0600

    removes multiple instantiations of vending machine object

--- src/test/scala/com/kylehall/vendingmachinetest/VendingMachineTest.scala
+++ src/test/scala/com/kylehall/vendingmachinetest/VendingMachineTest.scala
@@ -4,26 +4,24 @@ import com.kylehall.vendingmachine.VendingMachine
 
 class VendingMachineSpec extends UnitSpec {
 
+  def vendingMachine = new VendingMachine()
+
   "A VendingMachine" should "accept dimes and value them at 10 cents" in {
-    val vendingMachine = new VendingMachine()
     val total = vendingMachine.insertCoin("dime")
     assert(total == 10)
   }
 
   it should "accept nickels and value them at 5 cents" in {
-    val vendingMachine = new VendingMachine()
     val total = vendingMachine.insertCoin("nickel")
     assert(total == 5)
   }
 
   it should "accept quarters and value them at 25 cents" in {
-    val vendingMachine = new VendingMachine()
     val total = vendingMachine.insertCoin("quarter")
     assert(total == 25)
   }
 
   it should "reject pennies and value them at 0 cents" in {
-    val vendingMachine = new VendingMachine()
     val total = vendingMachine.insertCoin("penny")
     assert(total == 0)
   }

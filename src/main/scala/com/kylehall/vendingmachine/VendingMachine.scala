2ef856c49715657739c4291fec28035332d6d8e4
Kyle Hall <hallkd47@gmail.com>
2016-11-10 20:30:17 -0600

    changes all returned values to floats to match dollars and cents in domain

--- src/main/scala/com/kylehall/vendingmachine/VendingMachine.scala
+++ src/main/scala/com/kylehall/vendingmachine/VendingMachine.scala
@@ -2,12 +2,12 @@ package com.kylehall.vendingmachine
 
 class VendingMachine {
 
-  def insertCoin(coin: String): Int = {
-    var total = 0
-    if (coin == "dime") total += 10
-    else if (coin == "nickel") total += 5
-    else if (coin == "quarter") total += 25
-    else if (coin == "penny") total += 0
+  def insertCoin(coin: String): Float = {
+    var total = 0.0f
+    if (coin == "dime") total += 0.10f
+    else if (coin == "nickel") total += 0.05f
+    else if (coin == "quarter") total += 0.25f
+    else if (coin == "penny") total += 0.0f
     total
   }
 
--- src/test/scala/com/kylehall/vendingmachinetest/VendingMachineTest.scala
+++ src/test/scala/com/kylehall/vendingmachinetest/VendingMachineTest.scala
@@ -8,22 +8,22 @@ class VendingMachineSpec extends UnitSpec {
 
   "A VendingMachine" should "accept dimes and value them at 10 cents" in {
     val total = vendingMachine.insertCoin("dime")
-    assert(total == 10)
+    assert(total == 0.10f)
   }
 
   it should "accept nickels and value them at 5 cents" in {
     val total = vendingMachine.insertCoin("nickel")
-    assert(total == 5)
+    assert(total == 0.05f)
   }
 
   it should "accept quarters and value them at 25 cents" in {
     val total = vendingMachine.insertCoin("quarter")
-    assert(total == 25)
+    assert(total == 0.25f)
   }
 
   it should "reject pennies and value them at 0 cents" in {
     val total = vendingMachine.insertCoin("penny")
-    assert(total == 0)
+    assert(total == 0.0f)
   }
 
 }

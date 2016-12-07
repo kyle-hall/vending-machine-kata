e391504c78b9084f576af95dc305b85bf9528c11
Kyle Hall <hallkd47@gmail.com>
2016-12-06 20:48:22 -0600

    removes var from insert coins in favor of match expression

--- src/main/scala/com/kylehall/vendingmachine/VendingMachine.scala
+++ src/main/scala/com/kylehall/vendingmachine/VendingMachine.scala
@@ -2,13 +2,11 @@ package com.kylehall.vendingmachine
 
 class VendingMachine {
 
-  def insertCoin(coin: String, currentTotal: Float): Float = {
-    var newTotal = currentTotal
-    if (coin == "dime") newTotal += 0.10f
-    else if (coin == "nickel") newTotal += 0.05f
-    else if (coin == "quarter") newTotal += 0.25f
-    else newTotal += 0.0f
-    newTotal
+  def insertCoin(coin: String, currentTotal: Float): Float = coin match {
+    case "dime" => currentTotal + 0.10f
+    case "nickel" => currentTotal + 0.05f
+    case "quarter" => currentTotal + 0.25f
+    case _ => 0.0f
   }
 
   def returnCoins(currentTotal: Float): Float = {
